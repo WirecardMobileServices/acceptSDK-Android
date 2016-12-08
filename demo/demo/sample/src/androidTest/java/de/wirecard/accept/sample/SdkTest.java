1a68de74bf695b169c693f97ed826addfb7a7de0
mposSVK <mpos-svk@wirecard.com>
2016-12-08 07:26:39 +0100

    updated travisCI config file

--- demo/demo/sample/src/androidTest/java/de/wirecard/accept/sample/SdkTest.java
+++ demo/demo/sample/src/androidTest/java/de/wirecard/accept/sample/SdkTest.java
@@ -2,22 +2,13 @@ package de.wirecard.accept.sample;
 
 import android.test.AndroidTestCase;
 
-import de.wirecard.accept.extension.ThyronExtension;
 import de.wirecard.accept.sdk.AcceptSDK;
 
 public class SdkTest extends AndroidTestCase {
 
     public void testSdkExtensions() throws Exception {
         System.out.println("testSdkExtensions()");
-
         System.out.println("SDK version: " + AcceptSDK.getSDKVersion());
-
-//        System.out.println("SDK getSwiperType: " + AcceptSDK.getSwiperType());
-        System.out.println("SDK extension list: " + AcceptSDK.getSupportedExtensions());
-//        System.out.println("SDK getTerminalInfo: " + AcceptSDK.getTerminalInfo());
-
-        System.out.println("SDK ThyronExtension.THYRON_NAME: " + ThyronExtension.THYRON_NAME);
-
 
     }
 
